790dc29e13243fb9683ab64e347809dea72d0697
Shmuel Gibhin Zudik <ShmuelG@radware.com>
2021-06-10 13:56:54 +0300

    BP and API

--- ams/__init__.py
+++ ams/__init__.py
@@ -311,24 +311,24 @@ class Syslog(object):
 
 # class for Breaking Point
 class BP(object):
-        """
-        Class for Breaking Point
-        """
+    """
+    Class for Breaking Point
+    """
 
-        test_id = ""
+    test_id = ""
 
-        @staticmethod
-        def start(test: str, ip: str, user: str, password: str):
-            bps = BPS(ip, user, password)
-            # login
-            bps.login()
-            # showing current port reservation state
-            bps.portsState()
-            BP.test_id = bps.runTest(modelname=test, group=1)
-            bps.logout()
+    @staticmethod
+    def start(test: str, ip: str, user: str, password: str):
+        bps = BPS(ip, user, password)
+        # login
+        bps.login()
+        # showing current port reservation state
+        bps.portsState()
+        BP.test_id = bps.runTest(modelname=test, group=1)
+        bps.logout()
 
-        @staticmethod
-        def stop(ip: str, user: str, password: str, csv: bool = False):
+    @staticmethod
+    def stop(ip: str, user: str, password: str, csv: bool = False):
             try:
                 bps = BPS(ip, user, password)
                 # login
@@ -348,26 +348,26 @@ class BP(object):
 
 # class for Vision API
 class API(object):
-        """
-        Login/Logout/Get from Vision with REST API
-        """
-        # flag that indicate the success of the login to vision
-        flag = False
+    """
+    Login/Logout/Get from Vision with REST API
+    """
+    # flag that indicate the success of the login to vision
+    flag = False
 
-        def __init__(self, vision: str, user: str, password: str):
-            self.vision = vision
-            url = f"https://{self.vision}/mgmt/system/user/login"
-            fill_json = {"username": user, "password": password}
-            response = requests.post(url, verify=False, data=None, json=fill_json)
-            # self.flag = response.status_code
-            self.cookie = response.cookies
-            self.flag = False if "jsessionid" not in response.text else True
+    def __init__(self, vision: str, user: str, password: str):
+        self.vision = vision
+        url = f"https://{self.vision}/mgmt/system/user/login"
+        fill_json = {"username": user, "password": password}
+        response = requests.post(url, verify=False, data=None, json=fill_json)
+        # self.flag = response.status_code
+        self.cookie = response.cookies
+        self.flag = False if "jsessionid" not in response.text else True
 
-        def get(self, url: str):
-            response = requests.get(url, verify=False, data=None, cookies=self.cookie)
-            return response.json()
+    def get(self, url: str):
+        response = requests.get(url, verify=False, data=None, cookies=self.cookie)
+        return response.json()
 
-        def logout(self):
+    def logout(self):
             url = f"https://{self.vision}/mgmt/system/user/logout"
             response = requests.post(url, verify=False, cookies=self.cookie)
             # self.flag = response.status_code
