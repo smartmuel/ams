cdca2d74b198816051322c42a7b3f43bd3dba291
Shmuel Gibhin Zudik <ShmuelG@radware.com>
2021-09-16 15:23:07 +0300

    update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ams", # Replace with your own username
-    version="0.0.0",
+    version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
