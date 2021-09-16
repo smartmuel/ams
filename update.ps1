e70a888db2423a3199000991b6f5a7171acc35d8
Shmuel Gibhin Zudik <ShmuelG@radware.com>
2021-09-16 17:16:04 +0300

    0.0.1

--- update.ps1
+++ update.ps1
@@ -1,8 +1,11 @@
-$ver = python .\setup.py --version
-$array = $ver.Split('.')
-$array[2] = [string]([int]$array[2] +1)
-$newver = $array -join '.'
-(Get-Content .\setup.py).Replace($ver,$newver) | Out-File .\setup.py
-git add .
-if ( $args[0] -eq $null) {git commit -m "$ver"} else {git commit -m "$args[0]"}
-git push
+$change = git update-index --refresh
+if ( $change -ne $null) {
+	$ver = python .\setup.py --version
+	$array = $ver.Split('.')
+	$array[2] = [string]([int]$array[2] +1)
+	$newver = $array -join '.'
+	(Get-Content .\setup.py).Replace($ver,$newver) | Out-File .\setup.py
+	git add .
+	if ( $args[0] -eq $null) {git commit -m "$ver"} else {git commit -m "$args[0]"}
+	git push
+}
