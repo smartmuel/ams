93d0d37e85202f8da760d7d08d733e5d04d195be
Shmuel Gibhin Zudik <ShmuelG@radware.com>
2021-09-16 17:07:35 +0300

    test

--- update.ps1
+++ update.ps1
@@ -1,3 +1,8 @@
+$ver = python .\setup.py --version
+$array = $ver.Split('.')
+$array[2] = [string]([int]$array[2] +1)
+$newver = $array -join '.'
+(Get-Content .\setup.py).Replace($ver,$newver) | Out-File .\setup.py
 git add .
-if ( $args[0] -eq $null) {git commit -m "update"} else {git commit -m "$args[0]"}
+if ( $args[0] -eq $null) {git commit -m "$ver"} else {git commit -m "$args[0]"}
 git push
